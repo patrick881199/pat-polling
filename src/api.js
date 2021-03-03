5696ba406cec9bdf95a17b121279ede396232fda
Patrick <patrick881199@gmail.com>
2021-03-04 09:54:50 +1100

    change project name

--- src/api.js
+++ src/api.js
@@ -1,1 +1,3 @@
-export const base_url = "http://localhost:8081";
+// export const base_url = "http://localhost:8081";
+export const base_url =
+  "http://polling-app-server-dev2.us-west-2.elasticbeanstalk.com";
